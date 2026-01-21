d125bd45cafd112dd941ab57fbe2973950ce9ba5
gripxthebusiness-boop <gripxthebusiness@gmail.com>
2026-01-21 17:21:52 +0530

    Wrap app with CartProvider to fix cart blank screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
+
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import { Home } from './pages/Home';
-import { Products } from './pages/Products';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
-import { Cart } from './pages/Cart';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { ForgotPassword } from './pages/ForgotPassword';
-import { AdminProducts } from './pages/AdminProducts';
-import { Privacy } from './pages/Privacy';
-import { Terms } from './pages/Terms';
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <AuthProvider>
       <CartProvider>
         <Router>
-          <div className="App">
-            <Navigation />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/admin/products" element={<AdminProducts />} />
-              <Route path="/privacy" element={<Privacy />} />
-              <Route path="/terms" element={<Terms />} />
-            </Routes>
-            <Footer />
-          </div>
+          <Navigation />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+
+          <Footer />
         </Router>
       </CartProvider>
     </AuthProvider>
@@ -44,20 +39,3 @@ function App() {
 }
 
 export default App;
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import { Home } from './pages/Home';
-import { Products } from './pages/Products';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
-import  Cart  from './pages/Cart';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { ForgotPassword } from './pages/ForgotPassword';
-import { AdminProducts } from './pages/AdminProducts';
-import { Privacy } from './pages/Privacy';
-import { Terms } from './pages/Terms';
